14032ce6f29e7e365a5ed94dda3b2088e414aa14
Zack Tollman <tollmanz@gmail.com>
2015-08-30 14:16:43 -0700

    Critical CSS play.

--- http2/wp-content/mu-plugins/perf.php
+++ http2/wp-content/mu-plugins/perf.php
@@ -1,6 +1,22 @@
 <?php
 
 if ( ! is_admin() ) {
-    header( 'Link: <https://http2.wphttp.com/wp-content/themes/twentysixteen/style.css?ver=4.3>; rel=preload; as=stylesheet' );
+    header( 'Link: <https://http2.wphttp.com/wp-content/mu-plugins/perf/critical.css?ver=1>; rel=preload; as=script' );
     header( 'Link: <https://http2.wphttp.com/wp-includes/js/jquery/jquery.js?ver=1.11.3>; rel=preload; as=script' );
 }
+
+add_action( 'after_setup_theme', function() {
+    wp_deregister_style( 'twentysixteen-style' );
+    wp_enqueue_style(
+        'twentysixteen-style-critical',
+        plugins_url( 'perf/critical.css', __FILE__ ),
+        array(),
+        1
+    );
+}, 100 );
+
+add_action( 'wp_footer', function() {
+    ?>
+    <script id="twenty-sixteen-css">var ms=document.createElement("link");ms.rel="stylesheet";ms.href="https://http2.wphttp.com/wp-content/themes/twentysixteen/style.css?ver=4.3";document.getElementsByTagName("head")[0].appendChild(ms);</script>
+    <?php
+} );
